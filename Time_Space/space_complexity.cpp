795611d54ac1d8c6bffd7a2013eec6a5ebebd23c
Puneet-Kathpalia <puneetkathpalia99@gmail.com>
2024-06-10 23:24:59 +0530

    first commit

---
+++ Time_Space/space_complexity.cpp
@@ -0,0 +1,1 @@
+// space complexity measures how the memory or space required by an algorithm grows as the size of the input increases.
---
+++ Time_Space/time_complexity.cpp
@@ -0,0 +1,21 @@
+
+// time depends on the programming language
+// cpp is bit faster than the java
+
+// time also depends on the device 
+// as well as on the input
+
+// number of instructions performed by the programmer for giving the output
+
+// example finding the sum of n numbers
+// 1. using formula
+// 2. using loop
+
+
+// time complexity is the measure of how the running time of an algorithm grows as the size of input increases
+
+// n^0 O(1) constant time complexity doesnt depend on n ie input
+// O(n) linear time complexity depends on n ie input
+// in case of sun of n numbers
+// time complexity in using formula is constant
+// time complexity in using loop is linear
