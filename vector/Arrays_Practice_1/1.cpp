76f3b0a01b369ce76adc45d26018261cd6d257eb
Puneet-Kathpalia <puneetkathpalia99@gmail.com>
2024-06-13 23:20:16 +0530

    new file:   vector/Arrays_Practice_1/1.cpp

---
+++ vector/Arrays_Practice_1/1.cpp
@@ -0,0 +1,11 @@
+// fi(size==capacity)
+// then time complexity will be change 
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    
+    return 0;
+}
---
+++ vector/demo2.cpp
@@ -0,0 +1,14 @@
+//default intial cpacity is start from zero 
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    vector<int> v;
+
+    for(int i=0;i<20;i++){
+        cout<<v.size()<<' '<<v.capacity()<<'\n';
+        v.push_back(i);
+    }
+    return 0;
+}
