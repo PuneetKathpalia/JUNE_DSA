29b40ee084baed9f24c1a9fe8b48b5079c0f6be2
Puneet-Kathpalia <puneetkathpalia99@gmail.com>
2024-06-16 16:23:22 +0530

    new file:   Array_practice_2/max_difference_b/unique_occurence.cpp

---
+++ Array_practice_2/max_difference_b/unique_occurence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> frequency;
+        for(int x:arr){
+            frequency[x]++;
+        }
+        unordered_set<int> unique;
+        for (auto x: frequency){
+            unique.insert(x.second);
+        }
+        if(frequency.size()==unique.size()) return true;
+        return false;
+    }
+};
