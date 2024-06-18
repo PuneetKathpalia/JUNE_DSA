fc071ccf380e8afc84c3785ef55c4931df249611
Puneet-Kathpalia <puneetkathpalia99@gmail.com>
2024-06-18 12:21:43 +0530

    new file:   recursion/1ton.cpp

---
+++ recursion/1ton.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    void printn(int n,int i){
+        if(i==n+1)
+        return ;
+        cout<<i;
+        printn(n,i+1);
+    }
+int main(){
+    
+    return 0;
+}
