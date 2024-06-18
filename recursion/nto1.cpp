45cde2abea736bd58ad001a84b3c8e16b6db5937
Puneet-Kathpalia <puneetkathpalia99@gmail.com>
2024-06-18 12:30:11 +0530

    new file:   recursion/nto1.cpp 	new file:   recursion/nto1.exe

---
+++ recursion/nto1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    void printn(int n){
+        if(n==0)return ;
+        cout<<n<<endl;
+        printn(n-1);
+    }
+int main(){
+   printn(10);
+   
+}
