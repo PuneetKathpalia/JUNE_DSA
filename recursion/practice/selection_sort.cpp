bc1bb223005367d71c538a19396ecfb20b7a32a8
Puneet-Kathpalia <puneetkathpalia99@gmail.com>
2024-06-24 23:41:45 +0530

    modified:   recursion/practice/selection_sort.cpp

--- recursion/practice/selection_sort.cpp
+++ recursion/practice/selection_sort.cpp
@@ -1,60 +1,60 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
+// //{ Driver Code Starts
+// #include <bits/stdc++.h>
+// using namespace std;
+// void swap(int *xp, int *yp)
+// {
+//     int temp = *xp;
+//     *xp = *yp;
+//     *yp = temp;
+// }
 
 
-// } Driver Code Ends
-  class Solution
-{
-    public:
-    void selectionSort(int arr[], int n)
-    {
-       for(int i = 0; i < n; ++i) {
-        int min_id = i;
-        for(int j = i + 1; j < n; ++j)
-            if(arr[j] < arr[min_id])
-                min_id = j;
-        swap(arr[i], arr[min_id]);
-    }
-    }
-};
+// // } Driver Code Ends
+//   class Solution
+// {
+//     public:
+//     void selectionSort(int arr[], int n)
+//     {
+//        for(int i = 0; i < n; ++i) {
+//         int min_id = i;
+//         for(int j = i + 1; j < n; ++j)
+//             if(arr[j] < arr[min_id])
+//                 min_id = j;
+//         swap(arr[i], arr[min_id]);
+//     }
+//     }
+// };
 
-//{ Driver Code Starts.
+// //{ Driver Code Starts.
  
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
+// /* Function to print an array */
+// void printArray(int arr[], int size)
+// {
+//     int i;
+//     for (i=0; i < size; i++)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+// }
  
-// Driver program to test above functions
-int main()
-{
-    int arr[1000],n,T,i;
+// // Driver program to test above functions
+// int main()
+// {
+//     int arr[1000],n,T,i;
   
-    scanf("%d",&T);
+//     scanf("%d",&T);
     
-    while(T--){
+//     while(T--){
         
-    scanf("%d",&n);
+//     scanf("%d",&n);
     
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
+//     for(i=0;i<n;i++)
+//       scanf("%d",&arr[i]);
       
-    Solution ob;  
-    ob.selectionSort(arr, n);
-    printArray(arr, n);
-    }
-    return 0;
-}
+//     Solution ob;  
+//     ob.selectionSort(arr, n);
+//     printArray(arr, n);
+//     }
+//     return 0;
+// }
 
 // } Driver Code Ends
