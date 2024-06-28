2e78277cbdd40afc752ceea6d7139e81bc95e545
Puneet-Kathpalia <puneetkathpalia99@gmail.com>
2024-06-28 21:35:10 +0530

    modified:   recursion/practice/sqrtx.cpp

--- recursion/practice/bubble_sort.cpp
+++ recursion/practice/bubble_sort.cpp
@@ -1,63 +1,63 @@
-//{ Driver Code Starts
-// Initial Template for C++
+// //{ Driver Code Starts
+// // Initial Template for C++
 
-// C program for implementation of Bubble sort
-#include <bits/stdc++.h>
-#include <stdio.h>
-using namespace std;
+// // C program for implementation of Bubble sort
+// #include <bits/stdc++.h>
+// #include <stdio.h>
+// using namespace std;
 
-// swapping the elements
-void swap(int *xp, int *yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
+// // swapping the elements
+// void swap(int *xp, int *yp) {
+//     int temp = *xp;
+//     *xp = *yp;
+//     *yp = temp;
+// }
 
 
-// } Driver Code Ends
-// User function Template for C
-class Solution {
-  public:
-    // Function to sort the array using bubble sort algorithm.
-    void bubbleSort(int arr[], int n) {
-    for(int it = 1; it <= n - 1; ++it) {
+// // } Driver Code Ends
+// // User function Template for C
+// class Solution {
+//   public:
+//     // Function to sort the array using bubble sort algorithm.
+//     void bubbleSort(int arr[], int n) {
+//     for(int it = 1; it <= n - 1; ++it) {
         
-        for(int i = 0; i + 1 < n; ++i)
-            if(arr[i] > arr[i + 1])
-                swap(arr[i], arr[i+1]);
-    }
-    }
-};
+//         for(int i = 0; i + 1 < n; ++i)
+//             if(arr[i] > arr[i + 1])
+//                 swap(arr[i], arr[i+1]);
+//     }
+//     }
+// };
 
 
-//{ Driver Code Starts.
+// //{ Driver Code Starts.
 
-/* Function to print an array */
-void printArray(int arr[], int size) {
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
+// /* Function to print an array */
+// void printArray(int arr[], int size) {
+//     int i;
+//     for (i = 0; i < size; i++)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+// }
 
-// Driver program to test above functions
-int main() {
-    int arr[1000], n, T, i;
+// // Driver program to test above functions
+// int main() {
+//     int arr[1000], n, T, i;
 
-    scanf("%d", &T);
+//     scanf("%d", &T);
 
-    while (T--) {
+//     while (T--) {
 
-        scanf("%d", &n);
+//         scanf("%d", &n);
 
-        for (i = 0; i < n; i++)
-            scanf("%d", &arr[i]);
+//         for (i = 0; i < n; i++)
+//             scanf("%d", &arr[i]);
 
-        Solution ob;
+//         Solution ob;
 
-        ob.bubbleSort(arr, n);
-        printArray(arr, n);
-    }
-    return 0;
-}
+//         ob.bubbleSort(arr, n);
+//         printArray(arr, n);
+//     }
+//     return 0;
+// }
 // } Driver Code Ends
--- recursion/practice/sqrtx.cpp
+++ recursion/practice/sqrtx.cpp
@@ -1,12 +1,12 @@
-class Solution {
-public:
-    int mySqrt(int x) {
-        if (x == 0) return 0;
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         if (x == 0) return 0;
         
-        int sqrt;
-        for (sqrt = 1; sqrt <= x / sqrt; sqrt++) {
-        }
+//         int sqrt;
+//         for (sqrt = 1; sqrt <= x / sqrt; sqrt++) {
+//         }
         
-        return sqrt - 1;
-    }
-};
+//         return sqrt - 1;
+//     }
+// };
