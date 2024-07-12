df03aa657bfc01d56db79d933e5505a9dceb3ad1
PuneetKathpalia <119679009+PuneetKathpalia@users.noreply.github.com>
2024-07-12 07:40:56 +0530

    Add files via upload

---
+++ max_gain.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/maximum-score-from-removing-substrings/
+
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        if (x < y) {
+            swap(x, y);
+            for (char &c : s) {
+                if (c == 'a') c = 'b';
+                else if (c == 'b') c = 'a';
+            }
+        }
+
+        int points = 0;
+        stack<char> stk;
+        for (char c : s) {
+            if (!stk.empty() && stk.top() == 'a' && c == 'b') {
+                stk.pop();
+                points += x;
+            } else {
+                stk.push(c);
+            }
+        }
+
+        string remaining;
+        while (!stk.empty()) {
+            remaining += stk.top();
+            stk.pop();
+        }
+        reverse(remaining.begin(), remaining.end());
+
+        for (char c : remaining) {
+            if (!stk.empty() && stk.top() == 'b' && c == 'a') {
+                stk.pop();
+                points += y;
+            } else {
+                stk.push(c);
+            }
+        }
+
+        return points;
+    }
+};
