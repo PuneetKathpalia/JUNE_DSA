4f5517e7e17a184a4c3a3208ccc08eff62dbdb18
PuneetKathpalia <119679009+PuneetKathpalia@users.noreply.github.com>
2024-08-10 20:08:41 +0530

    Add files via upload

---
+++ node_class.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+	public:
+    int val;
+    Node* left;
+    Node* right;
+    Node() {
+        val = 0;
+        left = NULL;
+        right = NULL;
+    }
+    Node(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main() {
+	// your code goes here
+	return 0;
+}
