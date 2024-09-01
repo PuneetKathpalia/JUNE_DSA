848cd2330b9176ad2f81f14d4d001efb708fd31e
PuneetKathpalia <119679009+PuneetKathpalia@users.noreply.github.com>
2024-09-01 23:12:10 +0530

    Add files via upload

---
+++ declarationinitialistaion.cpp
@@ -0,0 +1,14 @@
+// string is a class
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    string city= "Ayodhya";
+    string name("Komal");
+    string address;
+    address="Jalandhar";
+    cout<<city<<endl<<name<<endl<<address<<endl;
+    cout<<name.size();
+    return 0;
+}
