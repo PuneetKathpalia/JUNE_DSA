a6f255ee9bce7c826445d3f6c66364bebe070e4c
PuneetKathpalia <119679009+PuneetKathpalia@users.noreply.github.com>
2024-09-04 23:53:40 +0530

    Add files via upload

---
+++ takinginputaccessing.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main(){
+    // string name;
+    // cin>>name;
+    // cout<<"name: "<<name;
+
+    // can not take full name
+    // string full_name;
+    // cin>>full_name;
+    // cout<<full_name;
+
+    // solution but after enter it will not read
+    // string full_name;
+    // getline(cin,full_name);
+    // cout<<full_name;
+
+
+    // string name;
+    // cin>>name;
+    // cout<<name[0]<<endl;
+    // name[0]='F';
+    // int length_of_name=name.size();
+    // for (int i=0;i<length_of_name;i++){
+    //     cout<<name[i]<<" ";
+    // }
+
+       // how multiple lines?????????
+    string para;
+    getline(cin,para,'d');
+    cout<<para;
+    return 0;
+}
